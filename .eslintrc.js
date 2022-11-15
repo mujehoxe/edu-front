d44a4cff498cb16eb362382ccd5e52eb1af31ce4
muje hoxe <mujehoxe@gmail.com>
2022-11-15 22:22:21 +0100

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:security/recommended',
+    'prettier',
+    './.eslintrc-auto-import.json',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2021,
+  },
+  rules: {
+    'no-var': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': ['error', 'only-multiline'],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '_' }],
+		'@typescript-eslint/no-var-requires': 0,
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
+}
