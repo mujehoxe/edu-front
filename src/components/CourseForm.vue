ca5d7fa43d64eb40e26dd901858ce2dba6c6f9d1
muje hoxe <mujehoxe@gmail.com>
2022-11-19 15:47:53 +0100

    refactor heroicons

--- src/components/CourseForm.vue
+++ src/components/CourseForm.vue
@@ -2,10 +2,11 @@
 import axios from 'axios';
 import { defineComponent } from 'vue';
 import ButtonComponent from './ButtonComponent.vue';
+import { XMarkIcon, PhotoIcon } from '@heroicons/vue/24/outline';
 
 
 export default defineComponent({
-    components: { ButtonComponent },
+    components: { ButtonComponent, XMarkIcon, PhotoIcon },
     emits: ["submit"],
     data() {
         return {
@@ -114,9 +115,7 @@ export default defineComponent({
             <div class="mt-1 sm:mt-0 sm:col-span-2">
               <div class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                 <div class="space-y-1 text-center">
-                  <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
+                  <PhotoIcon class="mx-auto h-12 w-12 text-gray-400"/>
                   <div class="flex items-center text-sm text-gray-600">
                     <label for="file" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                       <span>Upload a file</span>
@@ -127,9 +126,7 @@ export default defineComponent({
                   <div v-if="file.name" class="flex items-center justify-center text-xs text-gray-500">
                     {{ file.name }}
                     <button @click="onClearFile">
-                      <svg class="stroke-2 h-4 w-4 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                      </svg>
+                      <XMarkIcon class="stroke-2 h-4 w-4 text-indigo-600" />
                     </button>
                   </div>
                   <div v-else class="text-xs text-gray-500"> No file chosen </div>
