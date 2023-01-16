e127574e74a79f6b51221cbbe286432d94125d7b
muje hoxe <mujehoxe@gmail.com>
2023-01-16 09:11:50 +0100

    Photo Icon

--- src/pages/CourseForm.vue
+++ src/pages/CourseForm.vue
@@ -233,7 +233,7 @@ export default defineComponent({
                 <div class="mt-1 sm:mt-0 sm:col-span-2">
                   <div class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                     <div class="space-y-1 text-center">
-                      <PhotoIcon class="mx-auto h-12 w-12 text-gray-400"/>
+                      <PhotoIcon class="mx-auto h-12 w-12 text-gray-400" />
                       <div class="flex items-center justify-center text-sm text-gray-600">
                         <label for="file" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                           <span>Upload a file</span>
